6a0ce93b983d130a0288db48b1493be8988ba6b7
JADAV SAIKRISHNA <122225228+Jadav44@users.noreply.github.com>
2025-03-10 01:22:34 +0530

    Intial Commit

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from setuptools import find_packages, setup
 
 setup(
     name = 'Generative AI Project for Medical Domain',
-    version= '0.0.0',
+    version= '0.0.2',
     author= 'Jadav Saikrishna',
     author_email= 'jskrishnapatil@gmail.com',
     packages= find_packages(),
     install_requires = []
-
 )
